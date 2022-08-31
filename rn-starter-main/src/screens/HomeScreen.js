f3de2be97cb073ef56430e378276a11c2859a8ec
Sandil Hussain <sundiljutt618@gmail.com>
2022-08-31 22:27:57 +0500

    Revert "Goals app"
    This reverts commit 8283be67908a0f74ce335efded528ff84dc586dd.

---
+++ rn-starter-main/src/screens/HomeScreen.js
@@ -0,0 +1,83 @@
+import React from 'react';
+import { Text, StyleSheet, View, Button, TouchableOpacity } from 'react-native';
+
+const HomeScreen = ({navigation}) => {
+  console.log(navigation)
+  return (
+    <View>
+      <Text style={styles.text}>Hi There!</Text>
+      <Button
+        onPress={() => {
+          navigation.navigate('Components')
+          console.log('Button Pressed')
+        }}
+        title='Go to Components Demo'
+      />
+      <Button
+        onPress={() => {
+          navigation.navigate('List')
+          console.log('List Pressed')
+        }}
+        title='Go to List Demo'
+      />
+      <Button
+        onPress={() => {
+          navigation.navigate('Image')
+          console.log('List Pressed')
+        }}
+        title='Go to Image Demo'
+      />
+      <Button
+        onPress={() => {
+          navigation.navigate('Counter')
+          console.log('Counter Pressed')
+        }}
+        title='Go to Counter'
+      />
+      <Button
+        onPress={() => {
+          navigation.navigate('Color')
+          console.log('Color Pressed')
+        }}
+        title='Go to Color Demo'
+      />
+      <Button
+        onPress={() => {
+          navigation.navigate('Square')
+          console.log('Square Pressed')
+        }}
+        title='Go to Square Demo'
+      />
+      <Button
+        onPress={() => {
+          navigation.navigate('Text')
+          console.log('Text Pressed')
+        }}
+        title='Go to Text Demo'
+      />
+      <Button
+        onPress={() => {
+          navigation.navigate('Box')
+          console.log('Box Pressed')
+        }}
+        title='Go to Box Demo'
+      />
+      {/* <TouchableOpacity 
+        onPress={() => {
+          props.navigation.navigate('List')
+          console.log('List Pressed')
+        }}
+      >
+        <Text>Go to List Demo</Text>
+      </TouchableOpacity> */}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 30,
+  },
+});
+
+export default HomeScreen;
